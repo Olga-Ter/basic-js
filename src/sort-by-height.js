bcf913f62cb545434080ce4de423a7f1709b5ced
Olga <c_h_e_r_r_y@tut.by>
2024-12-21 12:56:38 +0300

    feat: task sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  function getMiddle(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    let mid = Math.trunc(sum / arr.length);
+    return mid;
+  }
+
+  let temparr = [];
+
+  if (arr.includes(-1)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== -1) temparr.push(arr[i]);
+    }
+
+    function quickSort(arr) {
+      if (arr.length < 2) {
+        return arr;
+      } else {
+        let middle = getMiddle(arr);
+        let left = [];
+        let right = [];
+        for (let i = 0; i < arr.length; i++) {
+          if (middle > arr[i]) {
+            left.push(arr[i]);
+          } else {
+            right.push(arr[i]);
+          }
+        }
+        return quickSort(left).concat(quickSort(right));
+      }
+    }
+    let temp = quickSort(temparr);
+    temparr = [];
+    let step = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== -1) {
+        temparr[i] = temp[step];
+        step++;
+      } else {
+        temparr[i] = arr[i];
+      }
+    }
+  } else {
+    temparr = arr.sort(function (a, b) {
+      return a - b;
+    });
+  }
+  return temparr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
